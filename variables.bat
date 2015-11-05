668e4d8a855811ef18fe74a71b4f5cc4aa3c5720
dgarcia2 <dgarciadom2@educacion.navarra.es>
2015-11-05 12:12:11 +0100

    Create variables.bat

---
+++ variables.bat
@@ -0,0 +1,18 @@
+:: variables.bat
+@echo off
+
+set NOVIA=Vane
+echo Hola, soy %NOVIA%
+echo Eres muy guapo
+echo.
+echo Recuerdame
+echo Soy %NOVIA%
+echo.
+set NOVIA=Rudolf
+set veces=10
+echo Hola, soy %NOVIA%
+echo Ayer te bese %veces% veces
+set veces=20
+echo Y te folle %veces%
+set NOVIA=%1
+:: toma un valor de la linea de comandos
