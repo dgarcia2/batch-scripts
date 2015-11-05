84a24d78b01aeae819a6aa2c3eacb21eac7b1a3b
dgarcia2 <dgarciadom2@educacion.navarra.es>
2015-11-05 12:10:53 +0100

    Create leerdato.bat

---
+++ leerdato.bat
@@ -0,0 +1,6 @@
+::leerdato.bat
+@echo off
+:: set nombre=%1 - Aquí tendríamos que añadir un argumento al ejecutar el script
+set /pnombre="Como te llamas??? "
+:: De la segunda forma, la consola nos preguntaría cómo nos llamamos de forma automática.
+echo Te llamas %nombre%
