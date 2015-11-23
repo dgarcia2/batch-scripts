99d235c7b038f99b5491ab702ee3203a75572074
dgarcia2 <dgarciadom2@educacion.navarra.es>
2015-11-23 13:38:47 +0100

    Update seleccion.bat

--- seleccion.bat
+++ seleccion.bat
@@ -3,10 +3,12 @@
 cls
 
 echo.
-CHOICE /c 1234 /N /M "Por favor, seleccione uno:"
+echo Por favor, seleccione una opción
 echo.
 echo 1. Iron Maiden
 echo 2. Metallica
 echo 3. Death
 echo 4. Ninguno de ellos
 echo.
+CHOICE /c 1234 /N /M "Su elección:"
+pause
