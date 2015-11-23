aea77367436a6ad5b001a76570230c2b8855b261
dgarcia2 <dgarciadom2@educacion.navarra.es>
2015-11-23 13:36:05 +0100

    Create seleccion.bat

---
+++ seleccion.bat
@@ -0,0 +1,12 @@
+:: seleccion.bat
+@echo off
+cls
+
+echo.
+CHOICE /c 1234 /N /M "Por favor, seleccione uno:"
+echo.
+echo 1. Iron Maiden
+echo 2. Metallica
+echo 3. Death
+echo 4. Ninguno de ellos
+echo.
