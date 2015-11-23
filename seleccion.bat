4ddc1d3f00ca75350fa18a18aec0db244b9694d6
dgarcia2 <dgarciadom2@educacion.navarra.es>
2015-11-23 13:47:10 +0100

    Update seleccion.bat

--- seleccion.bat
+++ seleccion.bat
@@ -1,5 +1,7 @@
 :: seleccion.bat
 @echo off
+:: Primero debemos mostrar la pantalla
+:: Limpiamos la pantalla...
 cls
 
 echo.
@@ -11,4 +13,27 @@ echo 3. Death
 echo 4. Ninguno de ellos
 echo.
 CHOICE /c 1234 /N /M "Su elección:"
+:: Para controlar lo introducido, se utiliza errorlevel
+:: en orden inverso
+if errorlevel 4 (
+	echo Ninguno de ellos te gusta.
+	goto fin
+)
+
+if errorlevel 3 (
+	echo El mejor grupo de la historia.
+	goto fin
+)
+
+if errorlevel 2 (
+	echo Metallica ya no son lo que eran.
+	goto fin
+)
+
+if errorlevel 1 (
+	echo Maiden siguen siendo los mejores.
+	goto fin
+)
+
+:fin
 pause
